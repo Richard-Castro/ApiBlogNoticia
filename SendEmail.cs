35ad036e7f66db1aef07cf7a3760248160edc7d2
Richard-Castro <richardcc540@gmail.com>
2023-04-13 22:04:06 -0300

    ppppppppppp

--- SendEmail.cs
+++ SendEmail.cs
@@ -18,7 +18,7 @@ namespace EnviaEmail
                 smtpclient.Credentials = new NetworkCredential("emerson.castro.ifsp@gmail.com", "zxcnfvpheahsscyu");
                 emailMessage.From = new MailAddress("emerson.castro.ifsp@gmail.com", "Emerson");
                 emailMessage.Body = "Testando o envio de email smtp pelo gmail";
-                emailMessage.Subject = "Teste envio - Castro";
+                emailMessage.Subject = "Teste envio - Castro9999";
                 emailMessage.IsBodyHtml = true;
                 emailMessage.Priority = MailPriority.Normal;
                 emailMessage.To.Add(email);
